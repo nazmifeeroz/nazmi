4fbabfcca783240b09b3d11eec927bc4ca73b9c4
Nazmi Feeroz <nazmifeeroz@hotmail.com>
2021-09-11 09:59:24 +0800

    :tada: small description about me

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+const figlet = require("figlet");
+
+figlet("Nazmi Feeroz", (_, data) => console.log(data));
+
+console.log(`
+    Nazmi Feeroz - Software Consultant
+
+    Github - https://github.com/nazmifeeroz/
+    Twitter: https://twitter.com/nazmifeeroz
+`);
