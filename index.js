9b255e477e6a8f36afd723f95a8595dfa6f61dc8
Nazmi Feeroz <nazmifeeroz@hotmail.com>
2021-09-24 09:51:24 +0800

    :lipstick: colour the finishing

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 #!/usr/bin/env node
 const figlet = require("figlet");
+const chalk = require("chalk");
 
-figlet("Nazmi Feeroz", (_, data) => console.log(data));
+figlet(" Nazmi Feeroz ", (_, data) => {
+  console.clear();
+  console.log(chalk.blue(data));
 
-console.log(`
-    Nazmi Feeroz - Software Consultant
+  console.log(`
+    ${chalk.blue.bold.underline("Nazmi Feeroz")} \u2022 ${chalk.bold.yellow(
+    "Software Consultant"
+  )}
 
-    Github - https://github.com/nazmifeeroz/
-    Twitter: https://twitter.com/nazmifeeroz
+    Nazmi is a full stack developer who is passionate about 
+    creating user-friendly and performant web applications. 
+    He is a strong advocate in clean code and managing states 
+    with state machines. 
+    
+    ${chalk.black.italic(" P.S.: He is handicap without Vim! ")}
+${chalk.dim(`
+    Github: ${chalk.white("  https://github.com/nazmifeeroz/  ")}
+    Twitter: https://twitter.com/nazmifeeroz`)}
 `);
+});
